1c7aedc77e786ee0c4780d7e318444658e89fdbf
KANG NING <38946092+Innovation0416@users.noreply.github.com>
2019-06-04 16:55:06 +0900

    fixed cart icon's postion

--- src/main/resources/static/js/touch-tools.js
+++ src/main/resources/static/js/touch-tools.js
@@ -5,12 +5,24 @@ $(window).on('touchmove', function() {
 		$('#side-bar').stop().animate({
 			opacity : "0.3"
 		}, 1);
+		$('#myCart').stop().animate({
+			opacity : "0.3"
+		}, 1);
+		$('.subscripts').stop().animate({
+			opacity : "0.3"
+		}, 1);
 	});
 	$(window).on('touchend', function() {
 		$('#amz-go-top').stop().animate({
 			opacity : "0.8"
 		}, 500);
 		$('#side-bar').stop().animate({
+			opacity : "0.8"
+		}, 500);
+		$('#myCart').stop().animate({
+			opacity : "0.8"
+		}, 500);
+		$('.subscripts').stop().animate({
 			opacity : "0.8"
 		}, 500);
 	});
